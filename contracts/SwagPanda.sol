39b318e16d932d3dcb6007500f2040352930b78d
Andrew Hartnett <ahartnett@insiderealestate.com>
2021-09-21 22:09:01 -0600

    Updating contract details

--- contracts/SwagPanda.sol
+++ contracts/SwagPanda.sol
@@ -11,6 +11,7 @@ contract SwagPanda is ERC721Enumerable, ReentrancyGuard, Ownable {
 
     uint256 public _currentTokenId = 1;
     uint256 public costToMint = 10000000000000000;
+    uint256 public supply = 1000;
 
     function tokenURI(uint256 tokenId) override public pure returns (string memory) {
         return string(abi.encodePacked("https://swagpanda.s3.amazonaws.com/meta/", Strings.toString((tokenId)), '.json'));
@@ -24,20 +25,24 @@ contract SwagPanda is ERC721Enumerable, ReentrancyGuard, Ownable {
     }
 
     function claim(uint256 _tokenId) payable public nonReentrant {
-        require(_tokenId < 2050, "tokenId invalid");
+        require(_tokenId < supply, "tokenId invalid");
         require(msg.value >= costToMint, 'less than costToMint');
         _safeMint(_msgSender(), _currentTokenId);
         _incrementTokenId();
     }
 
-    function ownerClaim() public nonReentrant onlyOwner {
-        require(_currentTokenId < 4201, "Token invalid");
-        _safeMint(owner(), _currentTokenId);
+    function ownerClaim(address _address) public nonReentrant onlyOwner {
+        require(_currentTokenId < supply, "Token invalid");
+        _safeMint(_address, _currentTokenId);
         _incrementTokenId();
     }
 
     function setCostToMint(uint256 _costToMint) public onlyOwner {
         costToMint = _costToMint;
+    }
+
+    function setSupply(uint256 _supply) public onlyOwner {
+        supply = _supply;
     }
 
     function withdraw() public onlyOwner {
