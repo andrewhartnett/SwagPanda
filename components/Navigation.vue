0340a88f65843e660135f67a3c20083ae9a64a62
Andrew Hartnett <ahartnett@insiderealestate.com>
2021-09-22 21:48:57 -0600

    Adding to nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,14 +1,15 @@
 <template>
     <nav class="flex justify-between my-4">
         <div class="w-1/3 text-2xl">
-            <nuxt-link to="/" class="text-purple-700 underline block">Swag Panda</nuxt-link>
+            <nuxt-link to="/" class="text-purple-700 hover:text-purple-900 underline block">Swag Panda</nuxt-link>
         </div>
         <div class="sm:flex sm:w-1/3 sm:justify-end">
             <!-- <nuxt-link to="/" class="text-purple-700 underline block">Opensea</nuxt-link> -->
-            <a href="https://twitter.com/AndyHartnett12" class="text-purple-700 underline block mx-4" target="_blank">Twitter</a>
+            <a href="https://github.com/andrewhartnett/SwagPanda" class="text-purple-700 hover:text-purple-900 underline block mx-4" target="_blank">Github</a>
+            <a href="https://twitter.com/AndyHartnett12" class="text-purple-700 hover:text-purple-900 underline block mx-4" target="_blank">Twitter</a>
             <!-- <nuxt-link to="/" class="text-purple-700 underline block">Discord</nuxt-link> -->
-            <a href="https://etherscan.io/address/0xbEff8D4e76239B1205d3E649D12097E79707De83#code" class="text-purple-700 underline block mx-4" target="_blank">Contract</a>
-            <a href="https://opensea.io/collection/swagpanda" class="text-purple-700 underline block mx-4" target="_blank">OpenSea</a>
+            <a href="https://etherscan.io/address/0xbEff8D4e76239B1205d3E649D12097E79707De83#code" class="text-purple-700 hover:text-purple-900 underline block mx-4" target="_blank">Contract</a>
+            <a href="https://opensea.io/collection/swagpanda" class="text-purple-700 hover:text-purple-900 underline block mx-4" target="_blank">OpenSea</a>
         </div>
     </nav>
 </template>
