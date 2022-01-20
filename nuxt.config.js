2ef3b87fa630b20632905cda8b6398e4db3647bf
Andrew Hartnett <ahartnett@insiderealestate.com>
2022-01-19 22:33:50 -0700

    SEO for pandas

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,30 +14,38 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'SwagPanda NFT. Swag Panda is an NFT collection that was built and opensourced for the purpose of learning ERC721 and NFTs in general.'
+      },
       { name: 'format-detection', content: 'telephone=no' },
       { name: 'twitter:card', content: 'summary' },
       { name: 'twitter:url', content: 'https://swagpanda.xyz' },
       { name: 'twitter:title', content: 'SwagPanda' },
-      { name: 'twitter:description', content: 'SwagPanda NFT' },
-      { name: 'twitter:image', content: 'https://swagpanda.xyz/SwagPandaLogo.png' },
-      { name: 'og:title', content: 'SwagPanda' },
+      {
+        name: 'twitter:description',
+        content:
+          'SwagPanda NFT. Swag Panda is an NFT collection that was built and opensourced for the purpose of learning ERC721 and NFTs in general.'
+      },
+      {
+        name: 'twitter:image',
+        content: 'https://swagpanda.xyz/SwagPandaLogo.png'
+      },
+      { name: 'og:title', content: 'Swag Panda' },
       { name: 'og:type', content: 'website' },
       { name: 'og:url', content: 'https://swagpanda.xyz' },
-      { name: 'og:image', content: 'https://swagpanda.xyz/SwagPandaLogo.png' },
+      { name: 'og:image', content: 'https://swagpanda.xyz/SwagPandaLogo.png' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -47,14 +55,12 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 }
